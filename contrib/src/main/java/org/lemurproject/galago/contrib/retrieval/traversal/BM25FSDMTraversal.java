6641e82ae01fbd6472850a4a8554b4dbff57e4c4
Fedor Nikolaev <fsqcds@gmail.com>
2017-06-07 20:09:15 -0400

    FSDM* and BM25FSDM traversals

---
+++ contrib/src/main/java/org/lemurproject/galago/contrib/retrieval/traversal/BM25FSDMTraversal.java
@@ -0,0 +1,265 @@
+// BSD License (http://www.galagosearch.org/license)
+package org.lemurproject.galago.contrib.retrieval.traversal;
+
+import org.apache.commons.lang.math.NumberUtils;
+import org.lemurproject.galago.core.index.stats.FieldStatistics;
+import org.lemurproject.galago.core.index.stats.NodeStatistics;
+import org.lemurproject.galago.core.retrieval.Retrieval;
+import org.lemurproject.galago.core.retrieval.ann.ImplementsOperator;
+import org.lemurproject.galago.core.retrieval.query.Node;
+import org.lemurproject.galago.core.retrieval.query.NodeParameters;
+import org.lemurproject.galago.core.retrieval.query.StructuredQuery;
+import org.lemurproject.galago.core.retrieval.traversal.Traversal;
+import org.lemurproject.galago.core.util.TextPartAssigner;
+import org.lemurproject.galago.utility.Parameters;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Transforms a #bm25f operator into a full expansion of the BM25F model. That
+ * means:
+ *
+ * Given `meg ryan war`, the output should be like:
+ *
+ * #bm25fcomb:K=0.5( #combine:0=0.407:1=0.382:2=0.187 (
+ * #feature:bm25f:b=0.1(meg.cast) #feature:bm25f:b=0.2(meg.team)
+ * #feature:bm25f:b=0.3(meg.title) ) #idf(meg) #combine:0=0.407:1=0.382:2=0.187
+ * ( #feature:bm25f:b=0.1(ryan.cast) #feature:bm25f:b=0.2(ryan.team)
+ * #feature:bm25f:b=0.3(ryan.title) ) #idf(ryan)
+ * #combine:0=0.407:1=0.382:2=0.187 ( #feature:bm25f:b=0.1(war.cast)
+ * #feature:bm25f:b=0.2(war.team) #feature:bm25f:b=0.3(war.title) ) #idf(war) )
+ *
+ * Except the weights involved should be tuned and not made up. Weights are:
+ *
+ * - b_f : A 'b' that is tuned for that field. It is NOT dependent to a query -
+ * only to the field. (attached to feature nodes) - W_f : A weight for each
+ * field, that is multiplied against every term for that field, making the total
+ * term weight. - K : A field/query independent tunable parameter. Used for
+ * smoothing term scores.
+ *
+ *
+ * @author irmarc
+ */
+@ImplementsOperator("bm25f")
+public class BM25FSDMTraversal extends Traversal {
+
+  List<String> fieldList;
+  Parameters availableFields, weights;
+  Retrieval retrieval;
+
+  public static final String UNIGRAM_FIELD_PREFIX = "uni-";
+  public static final String ORDERED_FIELD_PREFIX = "od-";
+  public static final String UNWINDOW_FIELD_PREFIX = "uww-";
+
+  private final int windowLimitDefault;
+  private final double unigramDefault;
+  private final double orderedDefault;
+  private final double unorderedDefault;
+
+  private final String odOp;
+  private final int odWidth;
+  private final String uwOp;
+  private final int uwWidth;
+
+
+  public BM25FSDMTraversal(Retrieval retrieval) {
+    this.retrieval = retrieval;
+    Parameters globals = retrieval.getGlobalParameters();
+    weights = globals.containsKey("bm25f") ? globals.getMap("bm25f") : Parameters.create();
+    fieldList = globals.getAsList("fields", String.class);
+    unigramDefault = globals.get("uniw", 0.8);
+    orderedDefault = globals.get("odw", 0.1);
+    unorderedDefault = globals.get("uww", 0.1);
+    windowLimitDefault = (int) globals.get("windowLimit", 2);
+
+    odOp = globals.get("sdm.od.op", "ordered");
+    odWidth = (int) globals.get("sdm.od.width", 1);
+
+    uwOp = globals.get("sdm.uw.op", "unordered");
+    uwWidth = (int) globals.get("sdm.uw.width", 8);
+
+    try {
+      availableFields = retrieval.getAvailableParts();
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public void beforeNode(Node original, Parameters queryParams) throws Exception {
+  }
+
+  public Node afterNode(Node original, Parameters queryParams) throws Exception {
+    if (original.getOperator().equals("bm25fsdm")) {
+      // Create the replacing root
+      NodeParameters rootP = new NodeParameters();
+      rootP.set("K", weights.get("K", queryParams.get("K", 0.5)));
+      Parameters cumulativeWeights = weights.get("weights", Parameters.create());
+      Parameters smoothing = weights.get("smoothing", Parameters.create());
+      Node uniRoot = new Node("bm25fcomb", rootP);
+      uniRoot.getNodeParameters().set("norm", false);
+      // Now generate the field-based subtrees for all extent/count nodes
+      // NOTE : THIS IS BROKEN. IT WON'T RECOGNIZE WINDOW COUNT NODES, BUT IT SHOULD
+      List<Node> children = original.getInternalNodes();
+      queryParams.set("numPotentials", children.size());
+      for (int i = 0; i < children.size(); i++) {
+        Node termNode = children.get(i);
+        double idf = getIDF(termNode);
+        Node termCombiner = createFieldsOfTerm(termNode, smoothing, cumulativeWeights, i, weights.get("K", 0.5),
+                idf, queryParams);
+        uniRoot.addChild(termCombiner);
+        uniRoot.getNodeParameters().set("idf" + i, idf);
+      }
+
+      int windowLimit = (int) queryParams.get("windowLimit", windowLimitDefault);
+
+      Node odRoot = new Node("bm25fcomb", rootP);
+      odRoot.getNodeParameters().set("norm", false);
+
+      Node uwRoot = new Node("bm25fcomb", rootP);
+      uwRoot.getNodeParameters().set("norm", false);
+
+      for (int n = 2; n <= windowLimit; n++) {
+        for (int i = 0; i < (children.size() - n + 1); i++) {
+          List<Node> seq = children.subList(i, i + n);
+          String orderedOp = this.odOp;
+          String unorderedOp = this.uwOp;
+
+          Node odNode = new Node(queryParams.get("sdm.od.op", orderedOp), new NodeParameters(queryParams.get("sdm.od.width", odWidth)), Node.cloneNodeList(seq));
+          Node uwwNode = new Node(queryParams.get("sdm.uw.op", unorderedOp), new NodeParameters(queryParams.get("sdm.uw.width", uwWidth)), Node.cloneNodeList(seq));
+
+          double odIdf = getIDF(odNode);
+          double uwwIdf = getIDF(uwwNode);
+
+          Node odCombiner = createFieldsOfBigram(seq, smoothing, cumulativeWeights, i, weights.get("K", 0.5),
+                  odIdf, queryParams, true);
+          Node uwwCombiner = createFieldsOfBigram(seq, smoothing, cumulativeWeights, i, weights.get("K", 0.5),
+                  uwwIdf, queryParams, false);
+
+
+          odRoot.addChild(odCombiner);
+          uwRoot.addChild(uwwCombiner);
+        }
+      }
+
+      NodeParameters weights = new NodeParameters();
+      ArrayList<Node> immediateChildren = new ArrayList<Node>();
+
+      double unigramW = queryParams.get("uniw", unigramDefault);
+      double orderedW = queryParams.get("odw", orderedDefault);
+      double unorderedW = queryParams.get("uww", unorderedDefault);
+
+      // unigrams
+      weights.set("0", unigramW);
+      immediateChildren.add(uniRoot);
+
+      // ordered
+      weights.set("1", orderedW);
+      immediateChildren.add(odRoot);
+
+      // unordered
+      weights.set("2", unorderedW);
+      immediateChildren.add(uwRoot);
+
+      return new Node("combine", weights, immediateChildren, original.getPosition());
+    } else {
+      return original;
+    }
+  }
+
+  private double getIDF(Node termNode) throws Exception {
+    // get the global document count:
+    FieldStatistics cs = retrieval.getCollectionStatistics("#lengths:part=lengths()");
+    double documentCount = cs.documentCount;
+
+    // get the number of documents this term occurs in:
+    termNode.getNodeParameters().set("queryType", "count");
+    termNode = retrieval.transformQuery(termNode, Parameters.create());
+    NodeStatistics ns = retrieval.getNodeStatistics(termNode.toString());
+    long df = ns.nodeDocumentCount;
+
+    // compute idf and return
+    return Math.log(documentCount / (df + 0.5));
+  }
+
+  private Node createFieldsOfTerm(Node termNode, Parameters smoothingWeights,
+          Parameters cumulativeWeights, int pos, double K, double idf, Parameters queryParams) throws Exception {
+    String term = termNode.getDefaultParameter();
+
+    // Use a straight weighting - no weight normalization
+    Node combiner = new Node("combine", new ArrayList<Node>());
+    combiner.getNodeParameters().set("norm", false);
+
+    for (String field : fieldList) {
+      // Actual count node
+      NodeParameters np = new NodeParameters();
+      np.set("default", term);
+      Node fieldTermNode = new Node("extents", np);
+      fieldTermNode = TextPartAssigner.assignFieldPart(fieldTermNode, availableFields, field);
+
+      // Now wrap it in the scorer
+      np = new NodeParameters();
+      np.set("b", smoothingWeights.get(field, queryParams.get("smoothing_" + field, weights.get("smoothing_default", 0.5))));
+      np.set("lengths", field);
+      np.set("pIdx", pos);
+      np.set("K", K);
+      np.set("idf", idf);
+      np.set("w", cumulativeWeights.get(field, queryParams.get("weight_" + field, weights.get("weight_default", 0.5))));
+      Node fieldScoreNode = new Node("bm25field", np);
+      fieldScoreNode.addChild(fieldTermNode);
+      combiner.getNodeParameters().set(Integer.toString(combiner.getInternalNodes().size()),
+              cumulativeWeights.get(field, queryParams.get("weight_" + field, weights.get("weight_default", 0.5))));
+      combiner.addChild(fieldScoreNode);
+    }
+
+    return combiner;
+  }
+
+  private Node createFieldsOfBigram(List<Node> seq, Parameters smoothingWeights,
+                                  Parameters cumulativeWeights, int pos, double K, double idf, Parameters queryParams, boolean ordered) throws Exception {
+
+    // Use a straight weighting - no weight normalization
+    Node combiner = new Node("combine", new ArrayList<Node>());
+    combiner.getNodeParameters().set("norm", false);
+
+    for (String field : fieldList) {
+      // Actual count node
+      NodeParameters np = new NodeParameters();
+
+      Node operationNode;
+      if (ordered)
+        operationNode = new Node(odOp, new NodeParameters(np.get("od.width", odWidth)));
+      else {
+        long unorderedWindow = np.get(("uw." + field + ".width"), np.get("uw.width", uwWidth));
+        operationNode = new Node(uwOp, new NodeParameters(unorderedWindow));
+      }
+
+
+      for (Node t : seq) {
+        String inFieldTerm = t.getNodeParameters().getAsSimpleString("default");
+        if (NumberUtils.isNumber(inFieldTerm)) inFieldTerm = "@/" + inFieldTerm + "/";
+        if (ordered)
+         operationNode.addChild(TextPartAssigner.assignFieldPart(StructuredQuery.parse("#extents:" + inFieldTerm + "()"), this.retrieval.getAvailableParts(), field));
+        else
+         operationNode.addChild(TextPartAssigner.assignFieldPart(StructuredQuery.parse("#extents:" + inFieldTerm + "()"), this.retrieval.getAvailableParts(), field));
+      }
+
+
+      // Now wrap it in the scorer
+      np = new NodeParameters();
+      np.set("b", smoothingWeights.get(field, queryParams.get("smoothing_" + field, weights.get("smoothing_default", 0.5))));
+      np.set("lengths", field);
+      np.set("pIdx", pos);
+      np.set("K", K);
+      np.set("idf", idf);
+      np.set("w", cumulativeWeights.get(field, queryParams.get("weight_" + field, weights.get("weight_default", 0.5))));
+      Node fieldScoreNode = new Node("bm25field", np);
+      fieldScoreNode.addChild(operationNode);
+      combiner.getNodeParameters().set(Integer.toString(combiner.getInternalNodes().size()),
+              cumulativeWeights.get(field, queryParams.get("weight_" + field, weights.get("weight_default", 0.5))));
+      combiner.addChild(fieldScoreNode);
+    }
+
+    return combiner;
+  }
+}
---
+++ contrib/src/main/java/org/lemurproject/galago/contrib/retrieval/traversal/FSDMStarTraversal.java
@@ -0,0 +1,296 @@
+package org.lemurproject.galago.contrib.retrieval.traversal;
+
+import org.apache.commons.lang.math.NumberUtils;
+import org.lemurproject.galago.core.index.stats.NodeStatistics;
+import org.lemurproject.galago.core.retrieval.Retrieval;
+import org.lemurproject.galago.core.retrieval.query.Node;
+import org.lemurproject.galago.core.retrieval.query.NodeParameters;
+import org.lemurproject.galago.core.retrieval.query.StructuredQuery;
+import org.lemurproject.galago.core.util.TextPartAssigner;
+import org.lemurproject.galago.utility.Parameters;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * For queries like "#fsdmstar:uw.attributes.width=8:uw.width=4(president barack obama)"
+ *
+ * One should specify field weights for unigrams and bigrams as Galago parameters with names:
+ *   --uni-<field name>
+ *   --od-<field name>
+ *   --uww-<field name>
+ *
+ * The total number of such parameters should be 3*Number of fields.
+ *
+ * Specify the names of fields to be used via a --fields parameter.
+ *
+ * @author Nikita Zhiltsov
+ */
+public class FSDMStarTraversal extends MLMTraversal {
+
+    public static final String ORDERED_FIELD_PREFIX = "od-";
+    public static final String UNWINDOW_FIELD_PREFIX = "uww-";
+
+    private final int windowLimitDefault;
+    private final double unigramDefault;
+    private final double orderedDefault;
+    private final double unorderedDefault;
+
+    private final String odOp;
+    private final int odWidth;
+    private final String uwOp;
+    private final int uwWidth;
+
+    public FSDMStarTraversal(Retrieval retrieval) {
+        super(retrieval);
+
+        unigramDefault = globals.get("uniw", 0.8);
+        orderedDefault = globals.get("odw", 0.1);
+        unorderedDefault = globals.get("uww", 0.1);
+        windowLimitDefault = (int) globals.get("windowLimit", 2);
+
+        odOp = globals.get("sdm.od.op", "ordered");
+        odWidth = (int) globals.get("sdm.od.width", 1);
+
+        uwOp = globals.get("sdm.uw.op", "unordered");
+        uwWidth = (int) globals.get("sdm.uw.width", 8);
+
+    }
+
+    @Override
+    public Node afterNode(Node original, Parameters qp) throws Exception {
+        if (original.getOperator().equals("fsdmstar")) {
+            return buildSDMNode(original, qp);
+        } else {
+            return original;
+        }
+    }
+
+    protected Node buildSDMNode(Node original, Parameters qp) throws Exception {
+        NodeParameters np = original.getNodeParameters();
+        List<Node> children = original.getInternalNodes();
+
+        NodeParameters sdmWeights = new NodeParameters();
+        List<Node> sdmNodes = new ArrayList<Node>();
+
+        addUnigramNodes(original, qp, np, children, sdmWeights, sdmNodes);
+
+        addBigramNodes(original, qp, np, children, sdmWeights, sdmNodes);
+
+        return new Node("combine", sdmWeights, sdmNodes, original.getPosition());
+    }
+
+    @Override
+    protected Node getUnigramNode(Node original, Parameters queryParameters, String term) throws Exception {
+        String scorerType = queryParameters.get("scorer", globals.get("scorer", "dirichlet"));
+
+        ArrayList<Node> termFields = new ArrayList<Node>();
+        NodeParameters nodeweights = new NodeParameters();
+        Parameters availableParts = this.retrieval.getAvailableParts();
+        int i = 0;
+        double normalizer = 0.0;
+        for (String field : fields) {
+            Node termFieldCounts, termExtents;
+
+            // if we have access to the correct field-part:
+            if (availableParts.containsKey("field.krovetz." + field) ||
+                    availableParts.containsKey("field.porter." + field) ||
+                    availableParts.containsKey("field." + field)) {
+                NodeParameters par1 = new NodeParameters();
+                par1.set("default", term);
+                termFieldCounts = TextPartAssigner.assignFieldPart(new Node("counts", par1, new ArrayList()), availableParts, field);
+            } else {
+                // otherwise use an #inside op
+                NodeParameters par1 = new NodeParameters();
+                par1.set("default", term);
+                termExtents = new Node("extents", par1, new ArrayList());
+                termExtents = TextPartAssigner.assignPart(termExtents, globals, availableParts);
+
+                termFieldCounts = new Node("inside");
+                termFieldCounts.addChild(StructuredQuery.parse("#extents:part=extents:" + field + "()"));
+                termFieldCounts.addChild(termExtents);
+            }
+
+//            double fieldWeight = 0.0;
+//            if (fieldWeights != null && fieldWeights.containsKey(UNIGRAM_FIELD_PREFIX + field)) {
+//                fieldWeight = fieldWeights.getDouble(UNIGRAM_FIELD_PREFIX + field);
+//            } else {
+//                fieldWeight = queryParameters.get(UNIGRAM_FIELD_PREFIX + field, 0.0);
+//            }
+
+            NodeStatistics ns = retrieval.getNodeStatistics(termFieldCounts);
+            double fieldWeight = (double) ns.nodeFrequency / (double) fieldStats.getFieldStats().get(field).collectionLength; // P(t|F_j)
+
+            nodeweights.set(Integer.toString(i), fieldWeight);
+            normalizer += fieldWeight;
+
+            Node termScore = new Node(scorerType);
+            termScore.getNodeParameters().set("lengths", field);
+            if (globals.containsKey("mu-" + field)) {
+                termScore.getNodeParameters().set("mu", globals.getDouble("mu-" + field));
+            }
+            termScore.addChild(fieldStats.getFieldLenNodes().get(field).clone());
+            termScore.addChild(termFieldCounts);
+            termFields.add(termScore);
+            i++;
+        }
+        // normalize field weights
+        if (normalizer != 0) {
+            for (i = 0; i < fields.size(); i++) {
+                String key = Integer.toString(i);
+                nodeweights.set(key, nodeweights.getDouble(key) / normalizer);
+            }
+        }
+
+        return new Node("wsum", nodeweights, termFields);
+    }
+
+    protected void addBigramNodes(Node original, Parameters qp, NodeParameters np, List<Node> children, NodeParameters sdmWeights, List<Node> sdmNodes) throws Exception {
+        int windowLimit = (int) qp.get("windowLimit", windowLimitDefault);
+        windowLimit = (int) np.get("windowLimit", windowLimit);
+        for (int n = 2; n <= windowLimit; n++) {
+            for (int i = 0; i < (children.size() - n + 1); i++) {
+                List<Node> termSequence = children.subList(i, i + n);
+                double odWeight = computeWeight(termSequence, np, qp, true);
+                BigramNodes bigramNodes = getBigramNodes(original, qp, termSequence);
+                if (odWeight != 0) {
+                    sdmWeights.set(Integer.toString(sdmNodes.size()), odWeight);
+                    sdmNodes.add(bigramNodes.ordered);
+                }
+                double uwWeight = computeWeight(termSequence, np, qp, false);
+                if (uwWeight != 0) {
+                    sdmWeights.set(Integer.toString(sdmNodes.size()), uwWeight);
+                    sdmNodes.add(bigramNodes.unordered);
+                }
+            }
+        }
+    }
+
+    protected BigramNodes getBigramNodes(Node original, Parameters qp, List<Node> seq) throws Exception {
+        NodeParameters np = original.getNodeParameters();
+
+        NodeParameters orderedFieldWeights = new NodeParameters();
+        double odNormalizer = 0.0;
+        NodeParameters unwindowFieldWeights = new NodeParameters();
+        double uwwNormalizer = 0.0;
+//        for (int i = 0; i < fields.size(); i++) {
+//            double odFieldWeight = 0.0;
+//            double uwdFieldWeight = 0.0;
+//            if (this.fieldWeights != null && this.fieldWeights.containsKey(ORDERED_FIELD_PREFIX + fields.get(i))) {
+//                odFieldWeight = this.fieldWeights.getDouble(ORDERED_FIELD_PREFIX + fields.get(i));
+//            } else {
+//                odFieldWeight = qp.get(ORDERED_FIELD_PREFIX + fields.get(i), 0.0);
+//            }
+//            if (this.fieldWeights != null && this.fieldWeights.containsKey(UNWINDOW_FIELD_PREFIX + fields.get(i))) {
+//                uwdFieldWeight = this.fieldWeights.getDouble(UNWINDOW_FIELD_PREFIX + fields.get(i));
+//            } else {
+//                uwdFieldWeight = qp.get(UNWINDOW_FIELD_PREFIX + fields.get(i), 0.0);
+//            }
+//            orderedFieldWeights.set(Integer.toString(i), odFieldWeight);
+//            odNormalizer += odFieldWeight;
+//            unwindowFieldWeights.set(Integer.toString(i), uwdFieldWeight);
+//            uwwNormalizer += uwdFieldWeight;
+//        }
+
+        String scorerType = qp.get("scorer", globals.get("scorer", "dirichlet"));
+        List<Node> orderedBigramFields = new ArrayList<Node>();
+        List<Node> unorderedBigramFields = new ArrayList<Node>();
+        for (int i = 0; i < fields.size(); i++) {
+            String field = fields.get(i);
+            Node orderedOperationNode = new Node(odOp, new NodeParameters(np.get("od.width", odWidth)));
+            long unorderedWindow = np.get(("uw." + field + ".width"), np.get("uw.width", uwWidth));
+            Node unorderedOperationNode = new Node(uwOp, new NodeParameters(unorderedWindow));
+            for (Node t : seq) {
+                String inFieldTerm = t.getNodeParameters().getAsSimpleString("default");
+                if (NumberUtils.isNumber(inFieldTerm)) inFieldTerm = "@/" + inFieldTerm + "/";
+                orderedOperationNode.addChild(TextPartAssigner.assignFieldPart(StructuredQuery.parse("#extents:" + inFieldTerm + "()"), this.retrieval.getAvailableParts(), field));
+                unorderedOperationNode.addChild(TextPartAssigner.assignFieldPart(StructuredQuery.parse("#extents:" + inFieldTerm + "()"), this.retrieval.getAvailableParts(), field));
+            }
+
+            NodeStatistics odNs = retrieval.getNodeStatistics(orderedOperationNode);
+            double odFieldWeight = (double) odNs.nodeFrequency / (double) fieldStats.getFieldStats().get(field).collectionLength; // P(t|F_j)
+            orderedFieldWeights.set(Integer.toString(i), odFieldWeight);
+            odNormalizer += odFieldWeight;
+
+            NodeStatistics uwdNs = retrieval.getNodeStatistics(unorderedOperationNode);
+            double uwdFieldWeight = (double) uwdNs.nodeFrequency / (double) fieldStats.getFieldStats().get(field).collectionLength; // P(t|F_j)
+            unwindowFieldWeights.set(Integer.toString(i), uwdFieldWeight);
+            uwwNormalizer += uwdFieldWeight;
+
+            Node orderedBigramScore = new Node(scorerType);
+            orderedBigramScore.getNodeParameters().set("lengths", field);
+            if (globals.containsKey("mu-" + field)) {
+                orderedBigramScore.getNodeParameters().set("mu", globals.getDouble("mu-" + field));
+            }
+            orderedBigramScore.addChild(fieldStats.getFieldLenNodes().get(field).clone());
+            orderedBigramScore.addChild(orderedOperationNode);
+            orderedBigramFields.add(orderedBigramScore);
+
+            Node unorderedBigramScore = new Node(scorerType);
+            unorderedBigramScore.getNodeParameters().set("lengths", field);
+            if (globals.containsKey("mu-" + field)) {
+                unorderedBigramScore.getNodeParameters().set("mu", globals.getDouble("mu-" + field));
+            }
+            unorderedBigramScore.addChild(fieldStats.getFieldLenNodes().get(field).clone());
+            unorderedBigramScore.addChild(unorderedOperationNode);
+            unorderedBigramFields.add(unorderedBigramScore);
+        }
+
+        // normalize field weights
+        if (odNormalizer != 0) {
+            for (int i = 0; i < fields.size(); i++) {
+                String key = Integer.toString(i);
+                orderedFieldWeights.set(key, orderedFieldWeights.getDouble(key) / odNormalizer);
+            }
+        } else {
+            for (int i = 0; i < fields.size(); i++) {
+                String key = Integer.toString(i);
+                orderedFieldWeights.set(key, 1.0);
+            }
+        }
+        if (uwwNormalizer != 0) {
+            for (int i = 0; i < fields.size(); i++) {
+                String key = Integer.toString(i);
+                unwindowFieldWeights.set(key, unwindowFieldWeights.getDouble(key) / uwwNormalizer);
+            }
+        } else {
+            for (int i = 0; i < fields.size(); i++) {
+                String key = Integer.toString(i);
+                unwindowFieldWeights.set(key, 1.0);
+            }
+        }
+
+        Node orderedNode = new Node("wsum", orderedFieldWeights, orderedBigramFields);
+        Node unorderedNode = new Node("wsum", unwindowFieldWeights, unorderedBigramFields);
+        return new BigramNodes(orderedNode, unorderedNode);
+    }
+
+    protected double computeWeight(String term, NodeParameters np, Parameters qp) throws Exception {
+        double unigramW = qp.get("uniw", unigramDefault);
+        unigramW = np.get("uniw", unigramW);
+        return unigramW;
+    }
+
+    protected double computeWeight(List<Node> bigram, NodeParameters np, Parameters qp, boolean isOrdered) throws Exception {
+        if (isOrdered) {
+            double orderedW = qp.get("odw", orderedDefault);
+            orderedW = np.get("odw", orderedW);
+            return orderedW;
+        } else {
+            double unorderedW = qp.get("uww", unorderedDefault);
+            unorderedW = np.get("uww", unorderedW);
+            return unorderedW;
+        }
+    }
+
+    protected class BigramNodes {
+        protected final Node ordered;
+        protected final Node unordered;
+
+        public BigramNodes(Node ordered, Node unordered) {
+            this.ordered = ordered;
+            this.unordered = unordered;
+        }
+    }
+
+}
