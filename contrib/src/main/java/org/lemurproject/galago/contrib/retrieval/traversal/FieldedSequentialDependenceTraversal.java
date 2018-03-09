3716b9a780fb856e78ea68c7b67e486578fd76fe
Fedor Nikolaev <fsqcds@gmail.com>
2018-03-08 21:24:48 -0500

    Fix setting of per-field mu and b in MLM and FSDM during learning

--- contrib/src/main/java/org/lemurproject/galago/contrib/retrieval/traversal/FieldedSequentialDependenceTraversal.java
+++ contrib/src/main/java/org/lemurproject/galago/contrib/retrieval/traversal/FieldedSequentialDependenceTraversal.java
@@ -157,11 +157,17 @@ public class FieldedSequentialDependenceTraversal extends MLMTraversal {
             }
             Node orderedBigramScore = new Node(scorerType);
             orderedBigramScore.getNodeParameters().set("lengths", field);
-            if (scorerType.equals("dirichlet") && globals.containsKey("mu-" + field)) {
-                orderedBigramScore.getNodeParameters().set("mu", globals.getDouble("mu-" + field));
+            if (scorerType.equals("dirichlet")) {
+                if (qp.containsKey("mu-" + field))
+                    orderedBigramScore.getNodeParameters().set("mu", qp.getDouble("mu-" + field));
+                else if (globals.containsKey("mu-" + field))
+                    orderedBigramScore.getNodeParameters().set("mu", globals.getDouble("mu-" + field));
             }
-            if (scorerType.equals("bm25") && globals.containsKey("smoothing_" + field)) {
-                orderedBigramScore.getNodeParameters().set("b", globals.getDouble("smoothing_" + field));
+            if (scorerType.equals("bm25")) {
+                if (qp.containsKey("smoothing_" + field))
+                    orderedBigramScore.getNodeParameters().set("b", qp.getDouble("smoothing_" + field));
+                else if (globals.containsKey("smoothing_" + field))
+                    orderedBigramScore.getNodeParameters().set("b", globals.getDouble("smoothing_" + field));
             }
             orderedBigramScore.addChild(fieldStats.getFieldLenNodes().get(field).clone());
             orderedBigramScore.addChild(orderedOperationNode);
